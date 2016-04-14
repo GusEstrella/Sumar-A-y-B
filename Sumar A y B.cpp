b8f433c5e2cd1092c71e04f3212826e09be590e5
GusEstrella <estrellagustavo100@gmail.com>
2016-04-14 00:01:29 -0300

    Added files via upload

---
+++ Sumar A y B.cpp
@@ -0,0 +1,23 @@
+/* El mayor entre A y B*/
+#include <iostream>
+
+int main() {
+
+std::cout << "--------- Sumemos A y B -------- \n\n\n";
+
+int a,b;
+
+//Ingresos del Usuario:
+std::cout << "Ingrese A: ";
+std::cin >> a;
+
+std::cout << "Ingrese B: "; 
+std::cin >> b;
+
+
+//Procesos del Programa:
+std::cout << "La Suma entre A y B es: "; 
+std::cout << a+b;
+
+
+}
